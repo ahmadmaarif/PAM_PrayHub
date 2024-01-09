c3e01b23ffbaf3c6f5bb5fa9422923a65b44aeec
ahmadmaarif <ahmad.maarif.ft21@mail.umy.ac.id>
2024-01-10 02:26:28 +0700

    Create PenggunaDao.kt

--- app/src/main/java/com/example/pam_prayhub/data/PenggunaDao.kt
+++ app/src/main/java/com/example/pam_prayhub/data/PenggunaDao.kt
@@ -1,4 +1,28 @@
 package com.example.pam_prayhub.data
 
-interface PenggunaDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SiswaPengguna {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertSiswa(siswa: Pengguna)
+
+    @Update
+    suspend fun updateSiswa(siswa: Pengguna)
+
+    @Delete
+    suspend fun deleteSiswa(siswa: Pengguna)
+
+    @Query("SELECT * from tblPengguna WHERE id = :id")
+    fun getSiswa (id: Int): Flow<Pengguna>
+
+    @Query("SELECT * from tblPengguna ORDER BY nama ASC")
+    fun getAllSiswa (): Flow<List<Pengguna>>
 }
