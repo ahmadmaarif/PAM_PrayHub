9cf68a28405333cf4ae5a9c62c9c2f7720f1f41f
ahmadmaarif <ahmad.maarif.ft21@mail.umy.ac.id>
2024-01-10 02:18:32 +0700

    Create dataclas Pengguna

--- app/src/main/java/com/example/pam_prayhub/data/Pengguna.kt
+++ app/src/main/java/com/example/pam_prayhub/data/Pengguna.kt
@@ -1,4 +1,12 @@
 package com.example.pam_prayhub.data
 
-class Pengguna {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblPengguna")
+data class Pengguna(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val email: String,
+)
