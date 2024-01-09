cbe9f54d5ea7bfb9ce93bc57e6c1b576bd2b9f88
ahmadmaarif <ahmad.maarif.ft21@mail.umy.ac.id>
2024-01-10 02:24:49 +0700

    Create DoaDao.kt

--- app/src/main/java/com/example/pam_prayhub/data/DoaDao.kt
+++ app/src/main/java/com/example/pam_prayhub/data/DoaDao.kt
@@ -1,4 +1,28 @@
 package com.example.pam_prayhub.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface DoaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertSiswa(siswa: Doa)
+
+    @Update
+    suspend fun updateSiswa(siswa: Doa)
+
+    @Delete
+    suspend fun deleteSiswa(siswa: Doa)
+
+    @Query("SELECT * from tbldoa WHERE id = :id")
+    fun getSiswa (id: Int): Flow<Doa>
+
+    @Query("SELECT * from tblDoa ORDER BY judul ASC")
+    fun getAllSiswa (): Flow<List<Doa>>
 }
