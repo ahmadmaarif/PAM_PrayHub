2c92db76658f603337f1dac66a88dc18cca5db81
ahmadmaarif <ahmad.maarif.ft21@mail.umy.ac.id>
2024-01-10 23:21:54 +0700

    add detailviewmodel

--- app/src/main/java/com/example/pam_prayhub/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pam_prayhub/repositori/ContainerApp.kt
@@ -1,2 +1,15 @@
 package com.example.pam_prayhub.repositori
 
+import android.content.Context
+import com.example.pam_prayhub.data.DatabasePrayHub
+
+interface ContainerApp {
+    val repositoriDoa : RepositoriDoa
+    val repositoriPengguna : RepositoriPengguna
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp {
+    override val repositoriDoa: RepositoriDoa by lazy {
+        OfflineRepositoriDoa(DatabasePrayHub.getDatabase(context).DoaDao())
+    }
+}
--- app/src/main/java/com/example/pam_prayhub/repositori/OfflineRepositori.kt
+++ app/src/main/java/com/example/pam_prayhub/repositori/OfflineRepositori.kt
@@ -1,2 +1,21 @@
 package com.example.pam_prayhub.repositori
 
+import kotlinx.coroutines.flow.Flow
+
+class OfflineRepositoriDoa(private val doaDao: Doa):RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> {
+        return siswaDao.getAllSiswa()
+    }
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
+    override suspend fun insertSiswa(siswa: Siswa) {
+        siswaDao.insertSiswa(siswa)
+    }
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.deleteSiswa(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.updateSiswa(siswa)
+
+
+}
