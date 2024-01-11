ef2cd3ec4ff6a86d613f79b903a06847e36b2307
ahmadmaarif <ahmad.maarif.ft21@mail.umy.ac.id>
2024-01-11 22:30:55 +0700

    UPDATE DoaEditViewModel.kt

--- app/src/main/java/com/example/pam_prayhub/halaman/DoaHalamanEdit.kt
+++ app/src/main/java/com/example/pam_prayhub/halaman/DoaHalamanEdit.kt
@@ -1,2 +1,54 @@
 package com.example.pam_prayhub.halaman
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.madoora.R
+import com.example.pam_prayhub.navigasi.DestinasiNavigasi
+
+import kotlinx.coroutines.launch
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_doa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () ->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold (
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(ItemEditDestination.titleRes),
+                canNavigateBack =  true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ){ innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.siswaUiState,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
--- app/src/main/java/com/example/pam_prayhub/model/DoaEditViewModel.kt
+++ app/src/main/java/com/example/pam_prayhub/model/DoaEditViewModel.kt
@@ -1,2 +1,53 @@
 package com.example.pam_prayhub.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_prayhub.halaman.ItemEditDestination
+import com.example.pam_prayhub.repositori.RepositoriDoa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriDoa: RepositoriDoa
+) : ViewModel(){
+
+    var doaUiState by mutableStateOf(UIStateDoa())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            doaUiState = repositoriDoa.getDoaStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateDoa(true)
+        }
+    }
+
+    suspend fun updateDoa(){
+        if (validasiInput(doaUiState.detailDoa)){
+            repositoriDoa.updateDoa(doaUiState.detailDoa.toDoa())
+        }
+        else {
+            println("Data tidak valid")
+        }
+    }
+
+    fun updateUiState(detailDoa: DetailDoa){
+        doaUiState =
+            UIStateDoa(detailDoa = detailDoa, isEntryValid = validasiInput(detailDoa))
+    }
+
+    private fun validasiInput(uiState: DetailDoa = doaUiState.detailDoa): Boolean {
+        return with(uiState){
+            judul.isNotBlank() && isi.isNotBlank() && tanggal.isNotBlank()
+        }
+    }
+}
